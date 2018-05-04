f8f97e2dd6b7724cf5800aeae5a9eb2dc5a45e75
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-05-04 19:15:20 +0200

    change Mat2

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,4 +1,7 @@
 #include "mat2.hpp"
+#include "vec2.hpp"
+#include <cmath>
+#include <iostream>
 
 
 Mat2::Mat2() : 
@@ -9,63 +12,72 @@ Mat2::Mat2(double a, double b, double c, double d) :
 
 
 Mat2& Mat2 :: operator*=(Mat2 const& m) {
-    Mat2 sum_multiply;
-    sum_multiply.arr[0] = ((arr[0] * m.arr[0]) + (arr[1] * m.arr[2]));
-    sum_multiply.arr[1] = ((arr[0] * m.arr[1]) + (arr[1] * m.arr[3]));
-    sum_multiply.arr[2] = ((arr[2] * m.arr[0]) + (arr[3] * m.arr[2]));
-    sum_multiply.arr[3] = ((arr[2] * m.arr[1]) + (arr[3] * m.arr[3]));
-    return sum_multiply;
+    Mat2 m1{*this};
+    arr[0] = ((m1.arr[0] * m.arr[0]) + (m1.arr[1] * m.arr[2]));
+    arr[1] = ((m1.arr[0] * m.arr[1]) + (m1.arr[1] * m.arr[3]));
+    arr[2] = ((m1.arr[2] * m.arr[0]) + (m1.arr[3] * m.arr[2]));
+    arr[3] = ((m1.arr[2] * m.arr[1]) + (m1.arr[3] * m.arr[3]));
+    return *this;
 }
-/*
+
+
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2) {
-    Mat2 sum_multiply;
-    sum_multiply.a[0] = (m1.a[0] * m2.a[0]) + (m1.a[1] * m2.a[2]);
-    sum_multiply.a[1] = (m1.a[0] * m2.a[1]) + (m1.a[1] * m2.a[3]);
-    sum_multiply.a[2] = (m1.a[2] * m2.a[0]) + (m1.a[3] * m2.a[2]);
-    sum_multiply.a[3] = (m1.a[2] * m2.a[1]) + (m1.a[3] * m2.a[3]);
+    Mat2 sum_multiply{m1};
+    sum_multiply*=m2;
     return sum_multiply;
 }
 
 Vec2 operator*(Mat2 const& m, Vec2 const& v) {
-    Mat2 vec_mutliply;
-    vec_multiply.x_ = (m.a[0]*v.x_)+(m.a[1]*v.y_);
-    vec_multiply.y_ = (m.a[2]*v.x_)+(m.a[3]*v.y_);
+    Vec2 vec_multiply;
+    vec_multiply.x_ = (m.arr[0]*v.x_)+(m.arr[1]*v.y_);
+    vec_multiply.y_ = (m.arr[2]*v.x_)+(m.arr[3]*v.y_);
     return vec_multiply;
 }
 
 Vec2 operator*(Vec2 const& v, Mat2 const& m){
-    Mat2 vec_mutliply;
-    vec_multiply.x_ = (m.a[0]*v.x_)+(m.a[1]*v.y_);
-    vec_multiply.y_ = (m.a[2]*v.x_)+(m.a[3]*v.y_);
+    Vec2 vec_multiply;
+    vec_multiply.x_ = (m.arr[0]*v.x_)+(m.arr[1]*v.y_);
+    vec_multiply.y_ = (m.arr[2]*v.x_)+(m.arr[3]*v.y_);
     return vec_multiply;
 }
 
-float Vec2 det() const {
-  float det = (a[0]*a[3])-(a[2]*a[1]);
+float Mat2::det() const {
+  float det = (arr[0]*arr[3])-(arr[2]*arr[1]);
   return det;
 }
 
 Mat2 inverse(Mat2 const& m) {
     double det = m.det();
-    if (det!=0) {
-        m.a[0]= (1/det)*(m.a[3]);
-        m.a[1]= (-1/det)*(m.a[1]);
-        m.a[2]= (-1/det)*(m.a[2]);
-        m.a[1]= (1/det)*(m.a[0]);
-        return m;
+    Mat2 m1{m};
+    if (det!= 0) {
+        m1.arr[0]= (1/det)*(m.arr[3]);
+        m1.arr[1]= (1/det)*(m.arr[1])*(-1);
+        m1.arr[2]= (1/det)*(m.arr[2])*(-1);
+        m1.arr[3]= (1/det)*(m.arr[0]);
     } else {
         std :: cout << "This Matrix doesn't have an inverse \n";
     }
+     return m1;
 }
 
 Mat2 transpose(Mat2 const& m) {
     Mat2 mTrans;
-    mTrans.a[0]= (m.a[0]);
-    mTrans.a[1]= (m.a[2]);
-    mTrans.a[2]= (m.a[1]);
-    mTrans.a[1]= (m.a[3]);
-    return m;
-}*/
+    mTrans.arr[0]= (m.arr[0]);
+    mTrans.arr[1]= (m.arr[2]);
+    mTrans.arr[2]= (m.arr[1]);
+    mTrans.arr[3]= (m.arr[3]);
+    return mTrans;
+}
 
-// Mat2 make_rotation_mat2(float phi); FEHLT
+Mat2 make_rotation_mat2(float phi) {
+    Mat2 mRotate;
+    mRotate.arr[0]= cos(phi);
+    mRotate.arr[1]= -sin(phi);
+    mRotate.arr[2]= sin(phi);
+    mRotate.arr[3]= cos(phi);
+    return mRotate;
+}
+
+
+
 
--- source/mat2.hpp
+++ source/mat2.hpp
@@ -1,6 +1,7 @@
 #ifndef MAT2_HPP
 #define MAT2_HPP
 #include <array>
+#include "vec2.hpp"
 
 struct Mat2 { 
     std::array<double, 4> arr;
@@ -13,10 +14,10 @@ struct Mat2 {
 
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2);
 
-/*Vec2 operator*(Mat2 const& m, Vec2 const& v);
+Vec2 operator*(Mat2 const& m, Vec2 const& v);
 Vec2 operator*(Vec2 const& v, Mat2 const& m);
 Mat2 inverse(Mat2 const& m);
 Mat2 transpose(Mat2 const& m);
 Mat2 make_rotation_mat2(float phi);
-*/
+
 #endif
